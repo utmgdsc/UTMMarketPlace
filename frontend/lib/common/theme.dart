6a118cc2f3b2fdb5f47e2c6ec4ee89e4ba57ad22
Ziad Zananiri <ziadzananirii@gmail.com>
2025-01-29 19:00:08 -0500

    Implemented basic UI and functionality for login page. Login page appears on start up of application.

---
+++ frontend/lib/common/theme.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+final appTheme = ThemeData(
+  colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+  useMaterial3: true,
+  scaffoldBackgroundColor: Colors.white,
+);
