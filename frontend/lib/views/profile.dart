6ca0fe0f3259e032da63185884c407de7c9f2d95
Marwan <marwan.yousef@mail.utoronto.ca>
2025-02-01 19:01:10 -0500

    removed print

--- frontend/lib/views/profile.dart
+++ frontend/lib/views/profile.dart
@@ -153,7 +153,6 @@ class _ProfileState extends State<Profile> {
                 GestureDetector(
                   onTap: () {
                     // TODO: Implement image picker
-                    print('Change profile picture');
                   },
                   child: Stack(
                     children: [
