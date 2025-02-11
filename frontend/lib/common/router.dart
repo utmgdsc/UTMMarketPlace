30df13898179ce8672e0880b37ab3a4fb6488f0d
marwan <marwan.yousef@mail.utoronto.ca>
2025-02-10 19:17:22 -0500

    [SCRUM-6] Implement profile page ui (#2)
    * completed changes to profile page
    
    * removed print
    
    * Prevent auto-formatting in branches
    
    * resolved suggestions and refactored code
    
    ---------
    
    Co-authored-by: Vallens <vallens.kho@gmail.com>

--- frontend/lib/common/router.dart
+++ frontend/lib/common/router.dart
@@ -1,9 +1,11 @@
 import 'package:go_router/go_router.dart';
 import 'package:utm_marketplace/views/login.dart';
+import 'package:utm_marketplace/views/profile.dart';
 
 // TODO: These are temporary imports for testing purposes, remove when no longer needed
 import 'package:utm_marketplace/views/model_view.dart';
 import 'package:utm_marketplace/models/model.dart';
+import 'package:utm_marketplace/views/home.dart';
 
 // Define the router as a top-level global variable
 final GoRouter router = GoRouter(
@@ -18,5 +20,18 @@ final GoRouter router = GoRouter(
       builder: (context, state) =>
           ModelView(model: Model(attribute1: 'Attribute 1', attribute2: 2)),
     ),
+    GoRoute(
+      path: '/profile/:userId',
+      builder: (context, state) {
+        final userId = state.pathParameters['userId'] ?? '';
+        final isOwnProfile = userId == 'me';
+        return Profile(userId: userId, isOwnProfile: isOwnProfile);
+      },
+    ),
+    GoRoute(
+      path: '/home',
+      builder: (context, state) => const HomePage(),
+    ),
   ],
 );
+
