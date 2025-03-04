fa22961eb1761d1bdc3fa9f27a7df507823b0cb5
ziadzananiri <ziadzananirii@gmail.com>
2025-03-03 19:25:10 -0500

    [SCRUM-14] Frontend Posting View Implementation (#14)
    * Set up MVVM foundation for the posting view.
    
    * Implemented the posting view as a component to the application.
    
    * Updated padding/flex for title and pricing on the posting view
    
    ---------
    
    Co-authored-by: Vallens Kho <69379333+V-X-L-U@users.noreply.github.com>

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -14,6 +14,8 @@ import 'package:utm_marketplace/menu/repository/menu.repository.dart';
 import 'package:utm_marketplace/menu/view_models/menu.viewmodel.dart';
 import 'package:utm_marketplace/profile/repository/profile.repository.dart';
 import 'package:utm_marketplace/profile/view_models/profile.viewmodel.dart';
+import 'package:utm_marketplace/posting_view/repository/posting.repository.dart';
+import 'package:utm_marketplace/posting_view/view_models/posting.viewmodel.dart';
 import 'package:utm_marketplace/create_listing/repository/create_listing.repository.dart';
 import 'package:utm_marketplace/create_listing/view_models/create_listing.viewmodel.dart';
 
@@ -40,6 +42,9 @@ Future<void> main() async {
         ),
         ChangeNotifierProvider(
           create: (_) => ProfileViewModel(repo: locator<ProfileRepository>()),
+        ),
+         ChangeNotifierProvider(
+          create: (_) => PostingViewModel(repo: locator<PostingRepository>()),
         ),
         ChangeNotifierProvider(
           create: (_) =>
---
+++ frontend/lib/posting_view/view/posting.view.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:utm_marketplace/posting_view/view_models/posting.viewmodel.dart';
+import 'package:utm_marketplace/shared/components/loading.component.dart';
+
+class PostingView extends StatefulWidget {
+  final String itemId;
+
+  const PostingView({super.key, required this.itemId});
+
+  @override
+  State<PostingView> createState() => _PostingViewState();
+}
+
+class _PostingViewState extends State<PostingView> {
+  late PostingViewModel viewModel;
+
+  @override
+  void initState() {
+    super.initState();
+    viewModel = Provider.of<PostingViewModel>(context, listen: false);
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      viewModel.fetchData(widget.itemId);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final loadingIndicator = const Center(child: LoadingComponent());
+
+    final itemUnavailable = const Center(child: Text('Item data unavailable.'));
+
+    return Scaffold(
+      body: SafeArea(
+        child: Consumer<PostingViewModel>(
+          builder: (_, postingViewModel, child) {
+            if (postingViewModel.isLoading) {
+              return loadingIndicator;
+            }
+            if (postingViewModel.item == null) {
+              return itemUnavailable;
+            }
+
+            final item = postingViewModel.item;
+            if (item == null || item.id != widget.itemId) {
+              return itemUnavailable;
+            }
+
+            final itemImage = item.imageUrl != null
+                ? Image.network(
+                    item.imageUrl!,
+                    width: double.infinity,
+                    height: 250,
+                    fit: BoxFit.cover,
+                  )
+                : Container();
+
+            final itemTitle = Text(
+              item.name,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+              overflow: TextOverflow.ellipsis,
+            );
+
+            final itemDetails = Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 8.0),
+                const Text(
+                  'Aubery Drake',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+                const SizedBox(height: 4.0),
+                const Text(
+                  'XX/XX/XXXX',
+                  style: TextStyle(
+                    fontSize: 16,
+                    color: Colors.grey,
+                  ),
+                ),
+              ],
+            );
+
+            final itemPrice = Text(
+              item.price == 0.0 ? 'FREE' : '\$${item.price.toStringAsFixed(2)}',
+              textAlign: TextAlign.right,
+              style: const TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            );
+
+            final itemDescription = Text(
+              item.description ?? 'No description available.',
+              style: const TextStyle(
+                fontSize: 16,
+              ),
+            );
+
+            return Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  itemImage,
+                  const SizedBox(height: 16.0),
+                  itemTitle,
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Expanded(flex: 4, child: itemDetails),
+                      Expanded(flex: 3, child: itemPrice),
+                    ],
+                  ),
+                  const SizedBox(height: 16.0),
+                  itemDescription,
+                ],
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
