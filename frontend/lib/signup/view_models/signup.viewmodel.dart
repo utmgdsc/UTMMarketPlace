c908edef9fe0c5e45768dca64751d45d02e0a477
Ziad Zananiri <ziadzananirii@gmail.com>
2025-03-09 17:01:43 -0400

    Slight fix to current signup viewmodel to handle how the backend endpoint response is currently setup

--- frontend/lib/signup/view_models/signup.viewmodel.dart
+++ frontend/lib/signup/view_models/signup.viewmodel.dart
@@ -28,10 +28,16 @@ class SignUpViewModel extends LoadingViewModel {
       _signupModel = SignUpModel(email: _email ?? '', password: _password ?? '');
       final response = await SignUpModel.signUp(_signupModel);
 
+      // TODO: When response status is cleaned up on backend, remove this if condition have a single line for signUpReponse assignment
       debugPrint('Sign up response: $response');
-      signUpResponse = response['statusCode'];
+      if (response.containsKey('user_id')) {
+        signUpResponse = 201;
+      } else {
+        signUpResponse = response['status_code'];
+      }
     } catch (exc) {
       debugPrint('Error in signUp: ${exc.toString()}');
+      signUpResponse = 500;
     } finally {
       isLoading = false;
       notifyListeners();
