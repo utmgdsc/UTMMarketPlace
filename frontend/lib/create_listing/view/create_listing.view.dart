d0779035c7641cfda4c26e3719ffae351c220a3b
Ziad Zananiri <ziadzananirii@gmail.com>
2025-03-19 00:06:26 -0400

    Snackbar notification for when a posting is successfully created or not

--- frontend/lib/create_listing/view/create_listing.view.dart
+++ frontend/lib/create_listing/view/create_listing.view.dart
@@ -62,7 +62,18 @@ class _CreateListingViewState extends State<CreateListingView> {
     );
 
     if (success && mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Posting successfully created!'),
+      ),
+      );
       context.pop();
+    } else if (mounted) {
+      ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('Error in posting. Please try again later.'),
+      ),
+      );
     }
   }
 
