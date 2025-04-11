fd789430ae3edf1c1f1c1f1dd6595aef9f7fd9b2
Vallens <vallens.kho@gmail.com>
2025-04-11 17:37:21 -0400

    Implement get search results

--- frontend/lib/item_listing/repository/listing_repo.dart
+++ frontend/lib/item_listing/repository/listing_repo.dart
@@ -1,9 +1,12 @@
+import 'package:dio/dio.dart';
+import 'package:utm_marketplace/item_listing/model/filter_options.model.dart';
+import 'package:utm_marketplace/item_listing/model/listing.model.dart';
 import 'package:utm_marketplace/shared/dio/dio.dart';
 
 class ListingRepo {
   final Map<String, dynamic> _cache = {};
 
-  Future<dynamic> fetchData(
+  Future<dynamic> getListings(
       {int limit = 6, String? nextPageToken, String? query}) async {
     final cacheKey = '$limit-$nextPageToken-$query';
     if (_cache.containsKey(cacheKey)) {
@@ -35,5 +38,41 @@ class ListingRepo {
     } catch (e) {
       throw Exception('Failed to load listings: $e');
     }
+  }
+
+  Future<ListingModel> getSearchResults(
+      {String? searchQuery,
+      int limit = 5,
+      String? nextPageToken,
+      FilterOptions? filterOptions}) async {
+    Response response;
+    try {
+      final queryParams = <String, String>{
+        if (searchQuery != null) 'query': searchQuery,
+        'limit': limit.toString(),
+        if (nextPageToken != null) 'next': nextPageToken,
+        if (filterOptions?.priceType != null)
+          'price_type': filterOptions!.priceType!,
+        if (filterOptions?.lowerPrice != null)
+          'lower_price': filterOptions!.lowerPrice!.toString(),
+        if (filterOptions?.upperPrice != null)
+          'upper_price': filterOptions!.upperPrice!.toString(),
+        if (filterOptions?.condition != null)
+          'condition': filterOptions!.condition!,
+        if (filterOptions?.dateRange != null)
+          'date_range': filterOptions!.dateRange!.toIso8601String(),
+        if (filterOptions?.campus != null) 'campus': filterOptions!.campus!,
+      };
+
+      response = await dio.get('/search', queryParameters: queryParams);
+
+      if (response.statusCode == 200) {
+        return ListingModel.fromJson(response.data);
+      }
+    } catch (e) {
+      throw Exception("Failed to load search results: $e");
+    }
+
+    throw Exception("Request failed with status code: ${response.statusCode}");
   }
 }
